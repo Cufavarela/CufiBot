897a38d3a6a428dee6747574534a2a303de628cc
Cufavarela <cufa.varela@gmail.com>
2022-01-07 19:11:40 -0300

    third

--- Cufibot.js
+++ Cufibot.js
@@ -3,38 +3,11 @@ import { Telegraf } from "telegraf";
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => {
-  ctx.reply("Hello " + ctx.from.first_name + "!");
+  ctx.reply("HODAAAAA " + ctx.from.first_name + "!");
 });
 
-bot.help((ctx) => {
-  ctx.reply("Send /start to receive a greeting");
-  ctx.reply("Send /keyboard to receive a message with a keyboard");
-  ctx.reply("Send /quit to stop the bot");
-});
-
-bot.command("quit", (ctx) => {
-  // Explicit usage
-  ctx.telegram.leaveChat(ctx.message.chat.id);
-  // Context shortcut
-  ctx.leaveChat();
-});
-
-bot.command("keyboard", (ctx) => {
-  ctx.reply(
-    "Keyboard",
-    Markup.inlineKeyboard([
-      Markup.button.callback("First option", "first"),
-      Markup.button.callback("Second option", "second"),
-    ])
-  );
-});
-
-bot.on("text", (ctx) => {
-  ctx.reply(
-    "You choose the " +
-      (ctx.message.text === "first" ? "First" : "Second") +
-      " Option!"
-  );
+bot.on("Hoda bebe", (ctx) => {
+  ctx.reply("Tau, mejor me voy a otro lado.");
 });
 
 bot.launch();
