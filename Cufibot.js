bff3d25569540b59c906b8b7cbb67bd6ff5351ca
Cufavarela <cufa.varela@gmail.com>
2022-01-07 19:01:06 -0300

    second

--- Cufibot.js
+++ Cufibot.js
@@ -1,13 +1,43 @@
 import { Telegraf } from "telegraf";
-//Prueba
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply("Hola Pau, soy el famoso CufiBot!!"));
-bot.help((ctx) => ctx.reply("Send me a sticker"));
-bot.on("Cufa es el mejor?", (ctx) => ctx.reply("Mas vale vieeeeeja 👍"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+
+bot.start((ctx) => {
+  ctx.reply("Hello " + ctx.from.first_name + "!");
+});
+
+bot.help((ctx) => {
+  ctx.reply("Send /start to receive a greeting");
+  ctx.reply("Send /keyboard to receive a message with a keyboard");
+  ctx.reply("Send /quit to stop the bot");
+});
+
+bot.command("quit", (ctx) => {
+  // Explicit usage
+  ctx.telegram.leaveChat(ctx.message.chat.id);
+  // Context shortcut
+  ctx.leaveChat();
+});
+
+bot.command("keyboard", (ctx) => {
+  ctx.reply(
+    "Keyboard",
+    Markup.inlineKeyboard([
+      Markup.button.callback("First option", "first"),
+      Markup.button.callback("Second option", "second"),
+    ])
+  );
+});
+
+bot.on("text", (ctx) => {
+  ctx.reply(
+    "You choose the " +
+      (ctx.message.text === "first" ? "First" : "Second") +
+      " Option!"
+  );
+});
+
 bot.launch();
 
-// Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
